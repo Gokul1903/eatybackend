3deb822e22b1abc4a3021533b57ab5b04ca3e102
Gokul1903 <rajgokul436@gmail.com>
2025-04-10 19:18:38 +0530

    changed cookie

--- eatycontroller/authcontroller.js
+++ eatycontroller/authcontroller.js
@@ -209,7 +209,7 @@ const Owner_signin = async (req,res)=>{
     res.cookie('token',token,{
         httpOnly:true,
         secure:process.env.NODE_ENV,
-        sameSite:'Strict',
+        sameSite:'None',
         maxAge:7 * 24 * 60 * 60 * 1000
 
     })
